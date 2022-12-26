69bdf8104bc062594557c85fc68ba533d3251e66
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-12-26 17:19:23 +0700

    intializing root to null

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,20 +6,34 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
-    class Program
+    class node
     {
-        /* A node class consi
-          
-          
-     */
-        class node
-        {
-            public string info;
-            public node lchild;
-            public node rchild;
+        public string info;
+        public node lchild;
+        public node rchild;
 
-            //constructor for the node class
+        //constructor for the node class
+
+        public node(string i, node l, node r)
+        {
+            info = i;
+            lchild = l;
+            rchild = r;
+
+
         }
+    }
+    class BinaryTree
+    {
+        public node ROOT;
+
+        public BinaryTree()
+        {
+            ROOT = null; // intializing root to null
+        }
+
+
+        
         static void Main(string[] args)
         {
         }
