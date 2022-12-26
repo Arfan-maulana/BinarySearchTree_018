27f3c6ee5ff5872d31a692b9bab775661a6be430
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-12-26 17:13:35 +0700

    constructor for the node class

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -12,6 +12,14 @@ namespace BinarySearchTree
           
           
      */
+        class node
+        {
+            public string info;
+            public node lchild;
+            public node rchild;
+
+            //constructor for the node class
+        }
         static void Main(string[] args)
         {
         }
